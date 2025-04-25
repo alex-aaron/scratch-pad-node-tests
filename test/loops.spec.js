dcffb24828e42d08cbad6cbbe9980561830b5dac
Alex Aaron <alexfrancisaaron@gmail.com>
2025-04-25 17:34:03 -0500

    created fizzBuzz and tests

--- test/loops.spec.js
+++ test/loops.spec.js
@@ -4,6 +4,7 @@ const chai = require('chai');
 const { assert, expect } = chai;
 const sinon = require('sinon');
 const loops = require("../day-2/loops");
+const { fizzBuzz } = require("../day-2/homework/fizzBuzz");
 const { 
   printArrayValues,
   printArrayValuesInReverse,
@@ -14,6 +15,108 @@ const {
   printObjectValuesInReverse,
   getObjectLength,
 } = loops;
+
+var fizzBuzzLogs = [
+  1,
+  2,
+  'Fizz',
+  4,
+  'Buzz',
+  'Fizz',
+  7,
+  8,
+  'Fizz',
+  'Buzz',
+  11,
+  'Fizz',
+  13,
+  14,
+  'FizzBuzz',
+  16,
+  17,
+  'Fizz',
+  19,
+  'Buzz',
+  'Fizz',
+  22,
+  23,
+  'Fizz',
+  'Buzz',
+  26,
+  'Fizz',
+  28,
+  29,
+  'FizzBuzz',
+  31,
+  32,
+  'Fizz',
+  34,
+  'Buzz',
+  'Fizz',
+  37,
+  38,
+  'Fizz',
+  'Buzz',
+  41,
+  'Fizz',
+  43,
+  44,
+  'FizzBuzz',
+  46,
+  47,
+  'Fizz',
+  49,
+  'Buzz',
+  'Fizz',
+  52,
+  53,
+  'Fizz',
+  'Buzz',
+  56,
+  'Fizz',
+  58,
+  59,
+  'FizzBuzz',
+  61,
+  62,
+  'Fizz',
+  64,
+  'Buzz',
+  'Fizz',
+  67,
+  68,
+  'Fizz',
+  'Buzz',
+  71,
+  'Fizz',
+  73,
+  74,
+  'FizzBuzz',
+  76,
+  77,
+  'Fizz',
+  79,
+  'Buzz',
+  'Fizz',
+  82,
+  83,
+  'Fizz',
+  'Buzz',
+  86,
+  'Fizz',
+  88,
+  89,
+  'FizzBuzz',
+  91,
+  92,
+  'Fizz',
+  94,
+  'Buzz',
+  'Fizz',
+  97,
+  98,
+  'Fizz',
+  'Buzz'];
 
 describe("loops", () => {
 
@@ -214,5 +317,21 @@ describe("loops", () => {
         });
       });
     });
+  });
+
+  describe("homework: fizzBuzz()", () => {
+    beforeEach(() => {
+      sinon.spy(console, 'log');
+    });
+    afterEach(() => {
+      console.log.restore();
+    });
+
+    it('should log the correct the value', () => {
+      fizzBuzz();
+      console.log.args.forEach((e, i) => {
+        assert.equal(e[0], fizzBuzzLogs[i]);
+      })
+    })
   });
 });
