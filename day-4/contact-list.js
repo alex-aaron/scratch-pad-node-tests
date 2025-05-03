ff70cf9a97a540948000c2dca247458d0aba2216
Alex Aaron <alexfrancisaaron@gmail.com>
2025-05-03 15:26:53 -0500

    created re-formatted contact-list.js

--- day-4/contact-list.js
+++ day-4/contact-list.js
@@ -1,4 +1,4 @@
-var data = [
+var contacts = [
   {
       "id": 1,
       "nameFirst": "Max",
@@ -52,50 +52,70 @@ var data = [
 ];
 
 /**
- * 4: Contact List // Contacts
+ * Create a factory function called makeContact(id, nameFirst, nameLast) that returns a contact object.
  * 
- *  a. Create a factory Function called makeContact(id, nameFirst, nameLast) 
- *     that returns a contact object.
- *     
- *          ex: makeContact(1, 'Max', 'Gaudin'); // => {id: 1, nameFirst: 'Max', nameLast: 'Gaudin'}
- *     
- *  b. Create a factory Function called makeContactList that returns an Object 
- *     that manages contacts. The contact-list object should have the following methods:
- *       
- *      1. length(): returns the number of contacts within the list.
- *      2. addContact(contact): takes a contact object to be added to the 
- *         contact-list.
- *      3. findContact(fullName): takes a full-name String, like 'Max Gaudin', and 
- *         returns the contact object if found in the contacts-list, or, 
- *         undefined if the fullName does not match any contacts in the list.
- *      4. removeContact(contact): takes a contact object to be removed from 
- *         the contact-list.
- *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should 
- *         return a String formated with all the full-names of the separated 
- *         with a line-break, like so:
- *          
- *         myContacts.printAllContactNames(); // => Max Gaudin
- *                                                  John Fraboni
- *                                                  Kaelyn Chresfield
- *          
- *          WARNING: To pass this test, the LAST full name should have NO
- *          new-line character added after it!
+ * Example:
+ *      makeContact(1, 'Max', 'Gaudin'); // => {id: 1, nameFirst: 'Max', nameLast: 'Gaudin'}
  */
 
 function makeContact(id, nameFirst, nameLast) {
-    
+	return {
+			id: id,
+			nameFirst: nameFirst,
+			nameLast: nameLast
+	};
 } 
 
+/**
+ * Create a function called length that returns the number of contacts in the contacts array.
+ */
+function length(){
+	return contacts.length;
+}
 
+/**
+ * Create a function called addContact that takes a contact object and adds it to the contacts
+ * array.
+ */
+function addContact(contact){
+	contacts.push(contact);
+}
 
-function makeContactList() {
-    /*
-     * You need something here to hold contacts. See length api for a hint:
-     */
-    var contacts = [];
-    
-    return {
-        // we implemented the length api for you //
-       
-    }
+/**
+ * Create a function called findContact that takes a full-name string, like "Max Gaudin", and returns
+ * the contact object in contacts that matches the full-name. If no match is found, the function should
+ * return undefined.
+ */
+function findContact(fullName){
+
+}
+
+/**
+ * Create a function called removeContact that takes in a contact object to be removed from
+ * the contacts array.
+ */
+function removeContact(contact){
+
+}
+
+/**
+ * Create a function called getAllContactNames that returns a string of every contact's full-name
+ * separated by a line-break character.
+ * 
+ * WARNING: To pass all of the tests, the final name in the output string should NOT have a line-break
+ * character.
+ */
+function getAllContactNames(){
+
+}
+
+// DO NOT REMOVE //
+module.exports = {
+    contacts: contacts,
+    makeContact: makeContact,
+    length: length,
+    addContact: addContact,
+    findContact: findContact,
+    removeContact: removeContact,
+    getAllContactNames: getAllContactNames
 }
